10d5245f32386fda8a7916c7d75db0506418b741
Gabriel <acostagabriel1311@gmai.com>
2023-05-18 13:17:16 -0700

    task 0 and betty changes

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef Hi
 #define Hi
 
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
