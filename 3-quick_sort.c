a4eda57be1c37db170379e8b3a5ab537051a9abc
Faqu22 <facundoalvarez415@gmail.com>
2023-05-19 17:28:50 -0300

    Three

---
+++ 3-quick_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+void quick_sort(int *array, size_t size)
+{
+size_t x = 0, z = 0;
+
+	int tmp = 0, *p = 0, cont = 0;
+
+	if (array == NULL)
+		return;
+
+	for (z = 0; z < size - 1; z++, cont = 0)
+	{
+	tmp = array[z];
+	for (x = z; x < size; x++)
+		if (array[x] < array[z])
+		{
+			p = &array[x];
+			array[z] = array[x];
+			cont++;
+		}
+	if (cont != 0)
+	{
+		*p = tmp;
+		print_array(array, size);
+	}
+	}
+}
