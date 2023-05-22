e644d0963e9049fe95ef87935f335e286e16dac3
Faqu22 <facundoalvarez415@gmail.com>
2023-05-22 14:41:20 -0300

    wewewe

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,34 +1,63 @@
 #include "sort.h"
 
 
+/**
+ * swap - swap
+ *
+ * @i: i.
+ * @j: j.
+ * @size: size.
+ * @array: array.
+ */
 void swap(int *i, int *j, size_t size, int *array)
 {
-    int tmp = 0;
-	
+	int tmp = 0;
+
 	if (*i != *j)
 	{
-		tmp = *i;
-    	*i = *j;
-    	*j = tmp;
-		print_array(array, size);
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+	print_array(array, size);
 	}
 }
 
+/**
+ * partition - partition.
+ *
+ * @array: array.
+ * @l: l.
+ * @r: r.
+ * @size: size.
+ *
+ * Return: returns the "pivot".
+ */
 int partition(int *array, int l, int r, size_t size)
 {
-    int pivot = array[r];
-    int i = l, j = l;
+	int pivot = array[r];
+	int i = l, j = l;
 
-    for (j = l; j < r; j++) {
-        if (array[j] < pivot) {
-            swap(&array[i], &array[j], size, array);
-            i++;
-        }
-    }
-    swap(&array[i], &array[r], size, array);
-    return (i);
+	for (j = l; j < r; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swap(&array[i], &array[j], size, array);
+			i++;
+		}
+	}
+	swap(&array[i], &array[r], size, array);
+	return (i);
 }
 
+
+/**
+ * quick_sort_Rec - quick sort.
+ *
+ * @array: array.
+ * @low: low.
+ * @high: high.
+ * @size: size.
+ */
 void quick_sort_Rec(int *array, int low, int high, size_t size)
 {
 	int i = 0;
@@ -41,6 +70,12 @@ void quick_sort_Rec(int *array, int low, int high, size_t size)
 	}
 }
 
+/**
+ * quick_sort - quick sort.
+ *
+ * @array: array.
+ * @size: size.
+ */
 void quick_sort(int *array, size_t size)
 {
 	quick_sort_Rec(array, 0, size - 1, size);
